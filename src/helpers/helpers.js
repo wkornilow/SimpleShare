9655270774043b3623967ff3a7ecd6a8964937ef
Denis Zhavoronok <the.wkornilow@gmail.com>
2018-01-24 15:11:38 +0200

    Refactoring

---
+++ src/helpers/helpers.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let ready = function ( fn ) {
+
+    // Sanity check
+    if ( typeof fn !== 'function' ) return;
+
+    // If document is already loaded, run method
+    if ( document.readyState === 'complete'  ) {
+        return fn();
+    }
+
+    // Otherwise, wait until document is loaded
+    document.addEventListener( 'DOMContentLoaded', fn, false );
+
+};
+
+export {ready};
